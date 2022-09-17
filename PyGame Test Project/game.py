cc761854ebb9c7bb3639386519ac47197ac23848
Shruti <sharmas0104@gmail.com>
2022-09-17 13:52:43 -0400

    updated move ball function

--- PyGame Test Project/game.py
+++ PyGame Test Project/game.py
@@ -26,21 +26,11 @@ goal2.turtlesize(stretch_wid=20, stretch_len=10)
 #ball setup
 ball = turtle.Turtle()
 ball.color("#cdd4c7")
-ball.shape("circle")
+ball.shape("triangle")
 ball.penup()
 ball.goto(0, 0)
 ball.turtlesize(1.5)
 
-#ball movement
-angle = random.randint(0, 360)
-ball_x = ball.xcor()
-ball_speed = 1000
-
-def ball_move_x():
-    x = ball.xcor()
-    ball.forward(ball_speed)
-    # x += ball_speed
-    # x = ball.setx(x)
 
 #player 1 setup
 goalie1 = turtle.Turtle()
@@ -84,35 +74,25 @@ def goalie1_move_down():
     goalie1.sety(y)
 
 
+def move_ball():
+
+    x_pos = random.randint(-500, 500)
+    y_pos = random.randint(-500,500)
+    ball.goto(x_pos, y_pos)
+
+
 screen.listen()
 screen.onkey(goalie2_move_up, 'Up')
 screen.onkey(goalie2_move_down, 'Down')
 screen.onkey(goalie1_move_up, 'w')
 screen.onkey(goalie1_move_down, 's')
 
+
 while True:
     screen.update()
-    ball_move_x()
 
-    if ball.xcor() > 300:
-        ball.setx(290)
-        ball.left(angle)
-        ball_speed = ball_speed * -1
-
-    elif ball.xcor() < -300:
-        ball.setx(-290)
-        ball.left(angle)
-        ball_speed = ball_speed * -1
-
-    if ball.ycor() > 300:
-        ball.sety(290)
-        ball.left(angle)
-        ball_speed = ball_speed * -1
-
-    elif ball.ycor() < -300:
-        ball.setx(-250)
-        ball.left(angle)
-        ball_speed = ball_speed * -1
+    while ball.xcor() in range(-1000, 1000) and ball.ycor() in range (-1000, 1000):
+        move_ball()
 
 
 
