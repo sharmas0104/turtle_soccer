cd9939ce3dd5c9ecb66f267f55cc98d695b9b9a9
Shruti <sharmas0104@gmail.com>
2022-09-18 09:19:12 -0400

    finished game

--- PyGame Test Project/game.py
+++ PyGame Test Project/game.py
@@ -26,19 +26,12 @@ goal2.turtlesize(stretch_wid=20, stretch_len=10)
 #ball setup
 ball = turtle.Turtle()
 ball.color("#cdd4c7")
-ball.shape("triangle")
+ball.shape("circle")
 ball.penup()
 ball.goto(0, 0)
 ball.turtlesize(1.5)
 
-#ball movement
-ball_x = ball.xcor()
-# ball_speed = 200
-speed = 100
 
-#pen score
-goalie1_score = 0
-goalie2_score = 0
 
 #pen
 pen = turtle.Turtle()
@@ -47,58 +40,80 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0, 300)
 pen.write("Goalie 1 : 0    Goalie 2: 0",
-             align="center", font=("Courier", 24, "normal"))
+          align="center", font=("Courier", 24, "normal"))
+
 
 def ball_move():
-    # ball_speed = random.randint(0, 350)
-    # x = ball.xcor()
-    # y = ball.ycor()
-    # while x in range(-350, 350):
-    #     ball.forward(ball_speed)
-    #     x += ball_speed
-    #     # y += ball_speed
-    #     ball.setx(x)
-    #     # y = ball.sety(y)
-    # if x >= 350:
-    #     #ball_speed *= -1
-    #     ball.backward(ball_speed)
-    #     x += ball_speed
-    #     ball.setx(x)
-    if(goalie2_score == 2 and goalie2_score > goalie1_score):
-        pen.clear()
-        pen.write("Goalie 2 won!", align="center", font=("Courier", 24, "normal"))
-        ball.goto(0,0)
-        return
-
-    if(goalie1_score == 2 and goalie1_score > goalie2_score):
-        pen.clear()
-        pen.write("Goalie 1 won!", align="center", font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        return
+    goalie1_score = 0
+    goalie2_score = 0
     speed = 100
-    sign = random.randint(0,1)
-    if sign == 1:
-        speed *= -1
-    while ball.xcor() in range(-350, 360):
-        ball.forward(speed)
+    if ball.pos() == (0, 0):
+        sign = random.randint(0, 1)
+        if sign == 1:
+            speed *= -1
 
-    if ball.xcor() <= -350:
-        #ball.forward(-speed)
-        ball.goto(350, random.randint(-200, 200))
-    if ball.xcor() >= 360:
-        #ball.backward(speed)
-        ball.goto(-350, random.randint(-200, 200))
+    while ball.xcor() in range(-700, 700):
+
+        y = ball.ycor()
+        dy = random.randint(-100, 100)
+        if y + dy in range(-200, 200):
+            y += dy
+        x = ball.xcor()
+        dx = speed
+        x += dx
+        ball.goto(x, y)
+
+        check_score(goalie1_score, goalie2_score)
+
+        if ((ball.xcor() < goalie1.xcor() + 60 and ball.xcor() > goalie1.xcor() - 60) and (ball.ycor() < goalie1.ycor() + 60 and ball.ycor() > goalie1.ycor() - 60)):
+            print("collision1")
+
+            speed = -speed
+            ball.forward(speed)
 
 
 
 
+        if ((ball.xcor() < goalie2.xcor() + 60 and ball.xcor() > goalie2.xcor() - 60) and (ball.ycor() < goalie2.ycor() + 60 and ball.ycor() > goalie2.ycor() - 60)):
+            print("collision2")
+
+            speed = -speed
+            ball.forward(speed)
+
+
+
+
+        if (ball.xcor() < -500):
+
+            goalie2_score += 1
+            pen.clear()
+            pen.write("Goalie 1 : {}    Goalie 2: {}".format(
+                goalie1_score, goalie2_score), align="center",
+                font=("Courier", 24, "normal"))
+
+
+            speed = -speed
+            ball.forward(speed)
+            ball.home()
+
+
+        if (ball.xcor() > 500):
+            goalie1_score += 1
+            pen.clear()
+            pen.write("Goalie 1 : {}    Goalie 2: {}".format(
+                goalie1_score, goalie2_score), align="center",
+                font=("Courier", 24, "normal"))
+            #ball.goto(-500, random.randint(-350, 350))
+            speed = -speed
+            ball.forward(speed)
+            ball.home()
 
 
 
 #player 1 setup
 goalie1 = turtle.Turtle()
 goalie1.color("#2914e3")
-goalie1.shape("circle")
+goalie1.shape("turtle")
 goalie1.penup()
 goalie1.goto(-390, 0)
 goalie1.turtlesize(3)
@@ -107,7 +122,7 @@ goalie1.left(90)
 #player 2 setup
 goalie2 = turtle.Turtle()
 goalie2.color("#2914e3")
-goalie2.shape("circle")
+goalie2.shape("turtle")
 goalie2.penup()
 goalie2.goto(390, 0)
 goalie2.turtlesize(3)
@@ -125,6 +140,7 @@ def goalie2_move_down():
     y -= 50
     goalie2.sety(y)
 
+
 def goalie1_move_up():
     y = goalie1.ycor()
     y += 50
@@ -136,8 +152,22 @@ def goalie1_move_down():
     y -= 50
     goalie1.sety(y)
 
-def is_collided_with(self, run):
-    return self.rect.colliderect(run.rect)
+
+def check_score(goalie1_score, goalie2_score):
+    if(goalie2_score == 15 and goalie2_score > goalie1_score):
+        pen.clear()
+        pen.write("Goalie 2 won!", align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.color("#e3d624")
+
+
+    if(goalie1_score == 15 and goalie1_score > goalie2_score):
+        pen.clear()
+        pen.write("Goalie 1 won!", align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.color("#e3d624")
+
+
 
 
 screen.listen()
@@ -146,69 +176,12 @@ screen.onkey(goalie2_move_down, 'Down')
 screen.onkey(goalie1_move_up, 'w')
 screen.onkey(goalie1_move_down, 's')
 
-ball_x = ball.xcor()
-g1_x = goalie1.xcor()
-ball_y = ball.ycor()
-g1_y = goalie1.xcor()
 
 run = True
 while run:
-    #angle = 72
+
     screen.update()
     ball_move()
-    if(goalie2_score == 2 and goalie2_score > goalie1_score):
-        pen.clear()
-        pen.write("Goalie 1 won!", align="center", font=("Courier", 24, "normal"))
-        time.sleep(5)
-        run = False
-    if(goalie1_score == 2 and goalie1_score > goalie2_score):
-        run = False
-
-
-
-    # #if ball.xcor() <= 350:
-    # if ball.xcor() in range(300, 350):
-    #     ball.setx(goalie1.xcor() + 50)
-    #     ball.setheading(ball.heading() + 180 + random.randint(0, 60))
-    #     ball_speed = ball_speed * -1
-    #
-    # if ball.xcor() >= -350:
-    #     ball.setx(goalie2.xcor() - 50)
-    #     #ball.left(angle)
-    #     ball.setheading(ball.heading() + 180 + + random.randint(0, 60))
-    #     ball_speed = ball_speed * -1
-    #
-    # if ball.ycor() > 340:
-    #     ball.sety(330)
-    #     #ball.left(angle)
-    #     ball.setheading(ball.heading() + 180 + random.randint(0, 60))
-    #     ball_speed = ball_speed * -1
-    #
-    # if ball.ycor() < -340:
-    #     ball.sety(-330)
-    #     #ball.left(angle)
-    #     ball.setheading(ball.heading() + 360 + random.randint(0, 60))
-    #     # time.sleep(2)
-    #     ball_speed = ball_speed * -1
-
-    if ((ball.xcor() < goalie1.xcor() + 60 and ball.xcor() > goalie1.xcor() - 60) and (ball.ycor() < goalie1.ycor() + 60 and ball.ycor() > goalie1.ycor() - 60)):
-        print("collision1")
-        goalie1_score+=1
-        pen.clear()
-        pen.write("Goalie 1 : {}    Goalie 2: {}".format(
-            goalie1_score, goalie2_score), align="center",
-            font=("Courier", 24, "normal"))
-        ball.goto(341, random.randint(-350, 350))
-
-
-    if ((ball.xcor() < goalie2.xcor() + 60 and ball.xcor() > goalie2.xcor() - 60) and (ball.ycor() < goalie2.ycor() + 60 and ball.ycor() > goalie2.ycor() - 60)):
-        print("collision2")
-        goalie2_score+=1
-        pen.clear()
-        pen.write("Goalie 1 : {}    Goalie 2: {}".format(
-            goalie1_score, goalie2_score), align="center",
-            font=("Courier", 24, "normal"))
-        ball.goto(-341, random.randint(-350, 350))
 
 
 
